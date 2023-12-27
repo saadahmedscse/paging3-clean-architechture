762a733b746faeed6c9ef134f36ec6f4ff1fa536
saadahmedscse <saadahmedscse@gmail.com>
2023-12-27 16:28:26 +0600

    created base paging adapter

--- app/src/main/java/com/saadahmedev/paging/ui/root/tabs/home/adapter/UnionAdapter.kt
+++ app/src/main/java/com/saadahmedev/paging/ui/root/tabs/home/adapter/UnionAdapter.kt
@@ -1,36 +1,16 @@
 package com.saadahmedev.paging.ui.root.tabs.home.adapter
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.paging.PagingDataAdapter
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
+import com.saadahmedev.paging.R
+import com.saadahmedev.paging.base.BasePagingAdapter
 import com.saadahmedev.paging.databinding.ItemUnionBinding
 import com.saadahmedev.paging.domain.model.UnionResponse
 
-class UnionAdapter : PagingDataAdapter<UnionResponse, UnionAdapter.UnionViewHolder>(UnionDiffCallback()) {
+class UnionAdapter : BasePagingAdapter<UnionResponse, ItemUnionBinding>() {
 
-    inner class UnionViewHolder(private val binding: ItemUnionBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(union: UnionResponse) {
-            binding.item = union
-        }
-    }
+    override val layoutRes: Int
+        get() = R.layout.item_union
 
-    class UnionDiffCallback : DiffUtil.ItemCallback<UnionResponse>() {
-        override fun areItemsTheSame(oldItem: UnionResponse, newItem: UnionResponse): Boolean {
-            return oldItem.id == newItem.id
-        }
-
-        override fun areContentsTheSame(oldItem: UnionResponse, newItem: UnionResponse): Boolean {
-            return oldItem == newItem
-        }
-    }
-
-    override fun onBindViewHolder(holder: UnionViewHolder, position: Int) {
-        getItem(position)?.let { holder.bind(it) }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UnionViewHolder {
-        return UnionViewHolder(ItemUnionBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    override fun onBind(binding: ItemUnionBinding, item: UnionResponse?, position: Int) {
+        binding.item = item
     }
 }
